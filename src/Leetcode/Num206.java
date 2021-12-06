48b33ed3baffeb52d7ae454a0a2d183937a83c3a
MatouSakura <1134979484@qq.com>
2021-12-06 23:51:42 +0800

    LeetCode206号问题,给你单链表的头节点 head ，请你反转链表，并返回反转后的链表,递归法翻转链表

--- src/Leetcode/Num206.java
+++ src/Leetcode/Num206.java
@@ -1,20 +1,31 @@
 package Leetcode;
 
 public class Num206 {
-    //1.原地移动法
     public ListNode reverseList(ListNode head) {
-        ListNode cur = head;
-        ListNode next = cur.next;
-        ListNode prev = null;
+        //1.原地移动法
+//        ListNode cur = head;
+//        ListNode next = cur.next;
+//        ListNode prev = null;
+//        if(head == null || head.next == null){
+//            return head;
+//        }
+//        while(cur != null){
+//            next = cur.next;//作为暂存节点,防止cur后的值丢失
+//            cur.next = prev;
+//            prev = cur;
+//            cur = next;
+//        }
+//        return prev;
+        //2.递归法
         if(head == null || head.next == null){
             return head;
         }
-        while(cur != null){
-            next = cur.next;//作为暂存节点,防止cur后的值丢失
-            cur.next = prev;
-            prev = cur;
-            cur = next;
-        }
-        return prev;
+        ListNode sec = head.next;
+        //翻转第二个节点之后的子链表
+        ListNode newHead = reverseList(head.next);
+        //将sec.next = head;
+        sec.next = head;
+        head.next = null;
+        return newHead;
     }
 }
