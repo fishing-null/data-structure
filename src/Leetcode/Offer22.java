6ebd23acbea1eee056e7d928374328357e1d9ed5
MatouSakura <1134979484@qq.com>
2021-12-08 16:38:52 +0800

    剑指offer22,输入一个链表，输出该链表中倒数第k个节点。为了符合大多数人的习惯，本题从1开始计数，即链表的尾节点是倒数第1个节点

---
+++ src/Leetcode/Offer22.java
@@ -0,0 +1,17 @@
+package Leetcode;
+
+public class Offer22 {
+    //双指针法
+    public ListNode getKthFromEnd(ListNode head, int k) {
+        ListNode fast = head;
+        for (int i = 0; i < k; i++) {
+            fast = fast.next;
+        }
+        ListNode low = head;
+        while(fast != null){
+            fast = fast.next;
+            low = low.next;
+        }
+        return low;
+    }
+}
