421135afb2fcdb3dd9ad3bd3270c271895e89be4
MatouSakura <1134979484@qq.com>
2021-12-08 21:08:30 +0800

    给你一个链表的头节点 head ，判断链表中是否有环。
      如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
    
      如果链表中存在环，则返回 true 。 否则，返回 false

---
+++ src/Leetcode/Num141.java
@@ -0,0 +1,17 @@
+package Leetcode;
+
+public class Num141 {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode low = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            low = low.next;
+            if(fast == low){
+                return true;
+            }
+        }
+        //出循环 走到了null
+        return false;
+    }
+}
