27b2670cd149b3c449554d004be94a57d29eb555
MatouSakura <1134979484@qq.com>
2021-12-10 13:45:45 +0800

    给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
    请必须使用时间复杂度为 O(log n) 的算法

---
+++ src/Leetcode/Num35.java
@@ -0,0 +1,23 @@
+package Leetcode;
+
+public class Num35 {
+    public static void main(String[] args) {
+        int[] nums ={1,3,5,6};
+        int target = 5;
+    }
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length-1;
+        while(left<=right){
+            int mid = (left+right)/2;
+            if(nums[mid] == target){
+                return mid;
+            }else if(target < nums[mid]){
+                right = mid-1;
+            }else if(target > nums[mid]){
+                left = mid+1;
+            }
+        }
+        return left;
+    }
+}
