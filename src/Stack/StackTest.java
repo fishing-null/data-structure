c35b3a6d9fa81f07283d461a75cadddd5080aa07
MatouSakura <1134979484@qq.com>
2022-01-23 18:22:20 +0800

    栈结构测试

---
+++ src/Stack/StackTest.java
@@ -0,0 +1,14 @@
+package Stack;
+
+public class StackTest {
+    public static void main(String[] args) {
+        MyStack stack = new MyStack();
+        stack.push(1);
+        stack.push(3);
+        stack.push(5);
+        System.out.println(stack);
+        stack.pop();
+        stack.peek();
+        System.out.println(stack);
+    }
+}
