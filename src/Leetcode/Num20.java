ba8f1bf3bd8ff94a7d895589b1b6cc6be5ad53a7
MatouSakura <1134979484@qq.com>
2022-01-24 20:17:04 +0800

    给定一个只包括 '('，')'，'{'，'}'，'['，']'的字符串 s ，判断字符串是否有效。
    有效字符串需满足：
    
    左括号必须用相同类型的右括号闭合。
    左括号必须以正确的顺序闭合。
    提示：
    
    1 <= s.length <= 104
    s 仅由括号 '()[]{}' 组成

---
+++ src/Leetcode/Num20.java
@@ -0,0 +1,36 @@
+package Leetcode;
+
+import java.util.Stack;
+
+public class Num20 {
+    public static void main(String[] args) {
+
+    }
+    public boolean isValid(String s) {
+        Stack<Character> s1 = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char charS = s.charAt(i);
+            if(charS == '{' || charS == '[' || charS == '('){
+                s1.push(charS);
+            }else{
+                if(s1.isEmpty()){
+                    return false;
+                }
+                char top = s1.pop();
+                if(top == '{' && charS !='}'){
+                    return false;
+                }
+                if(top == '[' && charS != ']'){
+                    return false;
+                }
+                if(top == '(' && charS != ')'){
+                    return false;
+                }
+            }
+        }
+        if(s1.isEmpty()){
+            return true;
+        }
+        return false;
+    }
+}
