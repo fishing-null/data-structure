bf60e9fc9c2d608a5dfda44fbd3c6f55f0d8ce24
MatouSakura <1134979484@qq.com>
2022-01-26 10:14:30 +0800

    给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。

---
+++ src/Leetcode/Num19.java
@@ -0,0 +1,20 @@
+package Leetcode;
+
+public class Num19 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummyHead = new ListNode(101);
+        dummyHead.next = head;
+        ListNode fast = dummyHead;
+        ListNode low = dummyHead;
+        //找到第倒数n+1个节点
+        for (int i = 0; i < n+1; i++) {
+            fast = fast.next;
+        }
+        while(fast != null){
+            fast = fast.next;
+            low = low.next;
+        }
+        low.next=low.next.next;
+        return dummyHead.next;
+    }
+}
