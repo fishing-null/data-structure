47c1353bd0ebc53068282693f2fc2decbf95ded1
MatouSakura <1134979484@qq.com>
2022-01-29 21:12:31 +0800

    给你一个数组 nums和一个值 val，你需要 原地 移除所有数值等于val的元素，并返回移除后数组的新长度。
    不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
    
    元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。

---
+++ src/Leetcode/Num27.java
@@ -0,0 +1,18 @@
+package Leetcode;
+
+public class Num27 {
+    public int removeElement(int[] nums, int val) {
+        int count = 0;
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i]!=val){
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+}
