a6f6255b191bda3295f415a352ecfde223c30412
MatouSakura <1134979484@qq.com>
2022-01-29 15:25:23 +0800

    反转链表

---
+++ src/Leetcode/Offer24.java
@@ -0,0 +1,14 @@
+package Leetcode;
+
+public class Offer24 {
+    public ListNode reverseList(ListNode head) {
+        ListNode dummyHead = new ListNode(-1);
+        while(head != null){
+            ListNode node = new ListNode(head.val);
+            node.next = dummyHead.next;
+            dummyHead.next = node;
+            head = head.next;
+        }
+        return dummyHead.next;
+    }
+}
