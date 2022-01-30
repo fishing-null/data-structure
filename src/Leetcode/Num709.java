83db7b449a550026a702ec24e0fcd551571df791
MatouSakura <1134979484@qq.com>
2022-01-30 18:40:47 +0800

    给你一个字符串 s ，将该字符串中的大写字母转换成相同的小写字母，返回新的字符串

--- src/Leetcode/Num709.java
+++ src/Leetcode/Num709.java
@@ -3,7 +3,24 @@ package Leetcode;
 import java.util.Locale;
 
 public class Num709 {
+    //1.
+//    public String toLowerCase(String s) {
+//        return s.toLowerCase();
+//    }
+    //2.
     public String toLowerCase(String s) {
-        return s.toLowerCase();
+        if(s == null){
+            return null;
+        }
+        char[] ch = s.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if(ch[i]>='A' && ch[i] <='Z'){
+                sb.append((char)(ch[i]+32));
+            }else{
+                sb.append(ch[i]);
+            }
+        }
+        return sb.toString();
     }
 }
