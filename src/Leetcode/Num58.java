bbb87bd9ba56087a34d6e13ceccb7df6073544a1
MatouSakura <1134979484@qq.com>
2022-01-31 16:09:21 +0800

    给你一个字符串 s，由若干单词组成，单词前后用一些空格字符隔开。返回字符串中最后一个单词的长度。
    单词 是指仅由字母组成、不包含任何空格字符的最大子字符串。

---
+++ src/Leetcode/Num58.java
@@ -0,0 +1,12 @@
+package Leetcode;
+
+public class Num58 {
+    public static void main(String[] args) {
+        lengthOfLastWord("Hello World");
+    }
+    public static int lengthOfLastWord(String s) {
+        String[] ret = s.split(" ");
+        String last = ret[ret.length-1];
+        return last.length();
+    }
+}
