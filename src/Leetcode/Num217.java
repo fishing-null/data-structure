55f5912d720d63903d12b9d0da78e918d76d9a9d
MatouSakura <1134979484@qq.com>
2022-02-01 16:02:12 +0800

    给你一个整数数组 nums 。如果任一值在数组中出现 至少两次 ，返回 true ；如果数组中每个元素互不相同，返回 false (双层循环,超出时间限制)

---
+++ src/Leetcode/Num217.java
@@ -0,0 +1,19 @@
+package Leetcode;
+
+public class Num217 {
+    class Solution {
+        //1.解法1,双层循环超时
+//        public boolean containsDuplicate(int[] nums) {
+//            for (int i = 0; i < nums.length; i++) {
+//                for (int j = nums.length-1; j > i; j--) {
+//                    if(nums[i] == nums[j]){
+//                        return true;
+//                    }
+//                }
+//            }
+//            return false;
+//        }
+        //2.
+
+    }
+}
