0288fec67ff618c8d158a445af9074d38bf7a5db
MatouSakura <1134979484@qq.com>
2022-02-04 17:34:04 +0800

    给定两个字符串, A 和 B。
    A 的旋转操作就是将 A 最左边的字符移动到最右边。 例如, 若 A = 'abcde'，在移动一次之后结果就是'bcdea' 。如果在若干次旋转操作之后，A 能变成B，那么返回True。

---
+++ src/Leetcode/Num796.java
@@ -0,0 +1,15 @@
+package Leetcode;
+
+import queue.Queue;
+
+public class Num796 {
+    class Solution {
+        public boolean rotateString(String s, String goal) {
+            if (s.length() != goal.length()) {
+                return false;
+            }
+            s += s;
+            return s.contains(goal)?true:false;
+        }
+    }
+}
