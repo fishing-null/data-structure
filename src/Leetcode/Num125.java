2484b701b6919ebdb56647c26aace4ed194c617a
MatouSakura <1134979484@qq.com>
2022-02-04 16:44:42 +0800

    给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
    说明：本题中，我们将空字符串定义为有效的回文串。

---
+++ src/Leetcode/Num125.java
@@ -0,0 +1,35 @@
+package Leetcode;
+
+import java.util.Locale;
+
+import static java.lang.Character.isLetter;
+
+public class Num125 {
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+        boolean ret = isPalindrome(s);
+        System.out.println(ret);
+    }
+    public static boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        int l = s.length();
+        StringBuilder str = new StringBuilder();
+        for(char c : s.toCharArray()){
+            if((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z')){
+                str.append(c);
+            }
+        }
+        String ret = str.toString();
+        int left = 0;
+        int right = ret.length() - 1;
+        while(left < right){
+            if(ret.charAt(left) == ret.charAt(right)){
+                left++;
+                right--;
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
