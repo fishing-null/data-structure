c079a8a15ebe5988b70554d9e41f61900385f61b
MatouSakura <1134979484@qq.com>
2022-02-06 09:15:02 +0800

    假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
    每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢)(本质是斐波那契数列问题)

---
+++ src/Leetcode/Num70.java
@@ -0,0 +1,19 @@
+package Leetcode;
+
+public class Num70 {
+    class Solution {
+        public int climbStairs(int n) {
+            if(n==1 || n==2){
+                return n;
+            }
+            int i1 = 1;
+            int i2 = 2;
+            for (int i = 3; i <=n; i++) {
+                int temp = i1+i2;
+                i1 = i2;
+                i2 = temp;
+            }
+            return i2;
+        }
+    }
+}
