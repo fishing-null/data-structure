f6ea8d0b92f8dabe062abc8662e6c7e4fc65aaca
MatouSakura <1134979484@qq.com>
2022-02-20 16:12:33 +0800

    二叉树的前 中 后序遍历(递归实现)

--- src/Leetcode/Num118.java
+++ src/Leetcode/Num118.java
@@ -5,9 +5,9 @@ import java.util.List;
 
 public class Num118 {
     public static void main(String[] args) {
-
+        System.out.println(generate(5));
     }
-    public  List<List<Integer>> generate(int numRows) {
+    public static List<List<Integer>> generate(int numRows) {
         List<List<Integer>> ans = new ArrayList<>(numRows);
         for (int i = 0; i < numRows; i++) {
             List<Integer> temp = new ArrayList<>(i+1);
---
+++ src/MyBiTree/MyBinTree.java
@@ -0,0 +1,64 @@
+package MyBiTree;
+
+public class MyBinTree {
+    private static class TreeNode{
+        char val;
+        TreeNode left;
+        TreeNode right;
+        public TreeNode(char val){
+            this.val = val;
+        }
+    }
+    public static TreeNode build(){
+        TreeNode nodeA = new TreeNode('A');
+        TreeNode nodeB = new TreeNode('B');
+        TreeNode nodeC = new TreeNode('C');
+        TreeNode nodeD = new TreeNode('D');
+        TreeNode nodeE = new TreeNode('E');
+        TreeNode nodeF = new TreeNode('F');
+        TreeNode nodeG = new TreeNode('G');
+        TreeNode nodeH = new TreeNode('H');
+        nodeA.left = nodeB;
+        nodeA.right = nodeC;
+        nodeB.left = nodeD;
+        nodeB.right = nodeE;
+        nodeE.right = nodeH;
+        nodeC.left = nodeF;
+        nodeC.right = nodeG;
+        return nodeA;
+    }
+    //先序遍历
+    public static void preOrder(TreeNode root){
+        // 边界条件
+        if(root == null){
+            return;
+        }
+        System.out.print(root.val+" ");
+        // 按照先序遍历的方式访问左树
+        preOrder(root.left);
+        // 按照先序遍历的方式访问右树
+        preOrder(root.right);
+    }
+    //中序遍历
+    public static void inOrder(TreeNode root){
+        // 边界条件
+        if(root == null){
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.val+" ");
+        inOrder(root.right);
+    }
+    //后序遍历
+    public static void postOrder(TreeNode root){
+        // 边界条件
+        if(root == null){
+            return;
+        }
+        // 按照后序遍历的方式访问左树
+        postOrder(root.left);
+        // 按照后序便利的方式访问右树
+        postOrder(root.right);
+        System.out.print(root.val+" ");
+    }
+}
