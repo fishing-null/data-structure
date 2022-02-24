9d54903de398ab0efcff42d88075dd0c42708c36
MatouSakura <1134979484@qq.com>
2022-02-24 22:34:00 +0800

    定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点.(递归解法)

--- src/Leetcode/Offer24.java
+++ src/Leetcode/Offer24.java
@@ -2,14 +2,25 @@ package Leetcode;
 import java.util.Stack;
 
 public class Offer24 {
-    public ListNode reverseList(ListNode head) {
-        ListNode dummyHead = new ListNode(-1);
-        while(head != null){
-            ListNode node = new ListNode(head.val);
-            node.next = dummyHead.next;
-            dummyHead.next = node;
-            head = head.next;
+//    public ListNode reverseList(ListNode head) {
+//        ListNode dummyHead = new ListNode(-1);
+//        while(head != null){
+//            ListNode node = new ListNode(head.val);
+//            node.next = dummyHead.next;
+//            dummyHead.next = node;
+//            head = head.next;
+//        }
+//        return dummyHead.next;
+//    }
+    public ListNode reverseList(ListNode head){
+        if(head == null || head.next == null){
+            return null;
         }
-        return dummyHead.next;
+        ListNode node = reverseList(head.next);
+        //将链表的头结点 递归的放到链表最后一个节点
+        head.next.next = head;
+        head.next = null;
+        return node;
     }
+
 }
