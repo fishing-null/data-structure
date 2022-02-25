e94fb74b7c0ba2d187ec6c006518ae89d870a406
MatouSakura <1134979484@qq.com>
2022-02-25 19:24:57 +0800

    二叉树的中序遍历(迭代写法)

--- src/Leetcode/Num94.java
+++ src/Leetcode/Num94.java
@@ -1,17 +1,34 @@
 package Leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Num94 {
+//    List<Integer> ret = new ArrayList<>();
+//    public List<Integer> inorderTraversal(TreeNode root) {
+//        if(root == null){
+//            return ret;
+//        }
+//        inorderTraversal(root.left);
+//        ret.add(root.val);
+//        inorderTraversal(root.right);
+//        return ret;
+//    }
     List<Integer> ret = new ArrayList<>();
-    public List<Integer> inorderTraversal(TreeNode root) {
+    public List<Integer> inorderTraversal(TreeNode root){
         if(root == null){
             return ret;
         }
-        inorderTraversal(root.left);
-        ret.add(root.val);
-        inorderTraversal(root.right);
+        TreeNode cur = root;
+        Stack<TreeNode> stack = new Stack<>();
+        while(cur != null || !stack.isEmpty()){
+            while(cur!=null){
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            ret.add(cur.val);
+            cur = cur.right;
+        }
         return ret;
     }
 }
