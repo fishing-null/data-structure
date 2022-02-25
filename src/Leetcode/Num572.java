ec739cd96f982204c9b2b8fed79511b210bbb51b
MatouSakura <1134979484@qq.com>
2022-02-25 17:27:35 +0800

    给你两棵二叉树 root 和 subRoot 。检验 root 中是否包含和 subRoot 具有相同结构和节点值的子树。如果存在，返回 true ；否则，返回 false 。
    二叉树 tree 的一棵子树包括 tree 的某个节点和这个节点的所有后代节点。tree 也可以看做它自身的一棵子树。

--- src/Leetcode/Num572.java
+++ src/Leetcode/Num572.java
@@ -18,15 +18,9 @@ public class Num572 {
         if(p == null && q == null){
             return true;
         }
-        if(p == null && !(q == null)){
+        if(p == null || q == null){
             return false;
         }
-        if(!(p == null) && q == null){
-            return false;
-        }
-        if(p.val != q.val){
-            return false;
-        }
-        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+        return p.val == q.val && isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
         }
     }
