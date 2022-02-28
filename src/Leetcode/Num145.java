5878b7491a5052c5d516ee11a8aaf1ba4a0616cb
MatouSakura <1134979484@qq.com>
2022-02-28 16:10:38 +0800

    二叉树的后序遍历(双栈实现)

--- src/Leetcode/Num145.java
+++ src/Leetcode/Num145.java
@@ -1,9 +1,6 @@
 package Leetcode;
 
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Num145 {
 //    List<Integer> ret = new ArrayList<>();
@@ -16,34 +13,59 @@ public class Num145 {
 //        ret.add(root.val);
 //        return ret;
 //    }
-      List<Integer> ret = new ArrayList<>();
-      public List<Integer> postorderTraversal(TreeNode root){
-          if(root == null){
-              return ret;
-          }
-          TreeNode cur = root;
-          //上一个完全处理过的节点(左右根都处理完毕的节点)
-          TreeNode prev = null;
-          Deque<TreeNode> queue = new LinkedList<>();
-          while(cur != null || !queue.isEmpty()){
-              while(cur != null){
-                  queue.push(cur);
-                  cur = cur.left;
-              }
-              //此时左树为空,cur取栈顶元素,第二次访问
-              cur = queue.pop();
-              //判断右树是否为空或者被我们访问过
-              if(cur.right == null || prev == cur.right){
-                  ret.add(cur.val);
-                  //当前节点cur就是最后处理的根节点,更新prev引用,变为cur
-                  prev = cur;
-                  cur = null;
-              }else{
-                  //此时右树不为空且没有处理过,就需要把根节点再压入栈中,处理右子树
-                  queue.push(cur);
-                  cur = cur.right;
-              }
-          }
-          return ret;
-      }
+//      List<Integer> ret = new ArrayList<>();
+//      public List<Integer> postorderTraversal(TreeNode root){
+//          if(root == null){
+//              return ret;
+//          }
+//          TreeNode cur = root;
+//          //上一个完全处理过的节点(左右根都处理完毕的节点)
+//          TreeNode prev = null;
+//          Deque<TreeNode> queue = new LinkedList<>();
+//          while(cur != null || !queue.isEmpty()){
+//              while(cur != null){
+//                  queue.push(cur);
+//                  cur = cur.left;
+//              }
+//              //此时左树为空,cur取栈顶元素,第二次访问
+//              cur = queue.pop();
+//              //判断右树是否为空或者被我们访问过
+//              if(cur.right == null || prev == cur.right){
+//                  ret.add(cur.val);
+//                  //当前节点cur就是最后处理的根节点,更新prev引用,变为cur
+//                  prev = cur;
+//                  cur = null;
+//              }else{
+//                  //此时右树不为空且没有处理过,就需要把根节点再压入栈中,处理右子树
+//                  queue.push(cur);
+//                  cur = cur.right;
+//              }
+//          }
+//          return ret;
+//      }
+    List<Integer> ret = new ArrayList<>();
+    public List<Integer> postorderTraversal(TreeNode root){
+        if(root == null){
+            return ret;
+        }
+        Stack<TreeNode> stack1 = new Stack<>();
+        Stack<Integer> stack2 = new Stack<>();
+        stack1.push(root);
+        while(!stack1.isEmpty()){
+            TreeNode cur = stack1.pop();
+            if(cur != null){
+                stack2.push(cur.val);
+            }
+            if(cur.left != null){
+                stack1.push(cur.left);
+            }
+            if(cur.right != null){
+                stack1.push(cur.right);
+            }
+        }
+        while(!stack2.isEmpty()){
+            ret.add(stack2.pop());
+        }
+        return ret;
+    }
 }
