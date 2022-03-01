f699777714826baa0e8c400186184b82afbcf178
MatouSakura <1134979484@qq.com>
2022-03-01 17:49:32 +0800

    给定两个整数数组 preorder 和 inorder ，其中 preorder 是二叉树的先序遍历， inorder 是同一棵树的中序遍历，请构造二叉树并返回其根节点。

---
+++ src/Leetcode/Num105.java
@@ -0,0 +1,32 @@
+package Leetcode;
+
+public class Num105 {
+    private int index;
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return bulidTreeHelper(preorder,inorder,0,preorder.length-1);
+    }
+
+    private TreeNode bulidTreeHelper(int[] preorder, int[] inorder, int left, int right) {
+        if(left>right){
+            return null;
+        }
+        if(index == preorder.length){
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[index]);
+        index++;
+        int pos = find(root.val,inorder);
+        root.left = bulidTreeHelper(preorder,inorder,left,pos-1);
+        root.right = bulidTreeHelper(preorder,inorder,pos+1,right);
+        return root;
+    }
+
+    private int find(int val, int[] inorder) {
+        for (int i = 0; i < inorder.length; i++) {
+            if(inorder[i] == val){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
