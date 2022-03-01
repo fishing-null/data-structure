b526b99739c94ab1d9eb6c260dbc7870638d4dc4
MatouSakura <1134979484@qq.com>
2022-03-01 20:47:51 +0800

    给定两个整数数组 inorder 和 postorder ，其中 inorder 是二叉树的中序遍历， postorder 是同一棵树的后序遍历，请你构造并返回这颗 二叉树 

--- src/Leetcode/Num106.java
+++ src/Leetcode/Num106.java
@@ -6,19 +6,18 @@ import java.util.Stack;
 
 public class Num106 {
     private int index;
-    List<Integer> postorderList = new ArrayList<>();
     public TreeNode buildTree(int[] inorder, int[] postorder) {
+        postorder =  reverse(postorder);
         return buildTreeHelper(inorder,postorder,0,postorder.length-1);
     }
     private TreeNode buildTreeHelper(int[] inorder, int[] postorder, int left, int right) {
-        reverse(postorder);
         if(left>right){
             return null;
         }
         if(index == postorder.length){
             return null;
         }
-        TreeNode root = new TreeNode(postorderList.get(index));
+        TreeNode root = new TreeNode(postorder[index]);
         index++;
         int pos = find(inorder, root.val);
         root.right = buildTreeHelper(inorder,postorder,pos+1,right);
@@ -34,9 +33,16 @@ public class Num106 {
         }
         return -1;
     }
-    private void reverse(int[] postorder){
-        for (int i = postorder.length-1; i >=0 ; i--) {
-            postorderList.add(postorder[i]);
+    private int[] reverse(int[] postorder){
+        int left = 0;
+        int right = postorder.length-1;
+        while(left<right){
+            int temp = postorder[left];
+            postorder[left] = postorder[right];
+            postorder[right] = temp;
+            left++;
+            right--;
         }
+        return postorder;
     }
 }
