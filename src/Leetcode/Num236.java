e95baebf87c28bc3053ee4c596ec990bf7c8c86d
MatouSakura <1134979484@qq.com>
2022-03-04 16:28:34 +0800

    给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
    百度百科中最近公共祖先的定义为：“对于有根树 T 的两个节点 p、q，最近公共祖先表示为一个节点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。

---
+++ src/Leetcode/Num236.java
@@ -0,0 +1,28 @@
+package Leetcode;
+
+public class Num236 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null){
+            return null;
+        }
+        if(root == p || root == q){
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left,p,q);
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
+        //都找到了
+        if(left != null && right != null){
+            return root;
+        }
+        //只找到了p
+        if(left != null){
+            return left;
+        }
+        //只找到了q
+        if(right != null){
+            return right;
+        }
+        //都没找到
+        return null;
+    }
+}
