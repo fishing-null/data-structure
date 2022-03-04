3167de94479857493cc44f8c4698bd8ed4034a87
MatouSakura <1134979484@qq.com>
2022-03-05 00:11:34 +0800

    给你二叉树的根结点 root ，请你将它展开为一个单链表：
    展开后的单链表应该同样使用 TreeNode ，其中 right 子指针指向链表中下一个结点，而左子指针始终为 null 。
    展开后的单链表应该与二叉树 先序遍历 顺序相同。

--- src/Leetcode/Num114.java
+++ src/Leetcode/Num114.java
@@ -1,18 +1,40 @@
 package Leetcode;
+import java.util.ArrayList;
 
+import java.util.List;
 public class Num114 {
+    private List<Integer> ret = new ArrayList<>();
     public void flatten(TreeNode root) {
+        //递归法
+//        if(root == null){
+//            return ;
+//        }
+//        flatten(root.left);
+//        flatten(root.right);
+//        TreeNode tmp = root.right;
+//        root.right = root.left;
+//        root.left = null;
+//        while(root.right != null){
+//            root = root.right;
+//        }
+//        root.right = tmp;
         if(root == null){
             return ;
         }
-        flatten(root.left);
-        flatten(root.right);
-        TreeNode tmp = root.right;
-        root.right = root.left;
+        preOrder(root);
         root.left = null;
-        while(root.right != null){
+        root.right = null;
+        for (int i = 1; i < ret.size(); i++) {
+            root.right = new TreeNode(ret.get(i));
             root = root.right;
         }
-        root.right = tmp;
+    }
+    private void preOrder(TreeNode root){
+        if(root == null){
+            return;
+        }
+        ret.add(root.val);
+        preOrder(root.left);
+        preOrder(root.right);
     }
 }
