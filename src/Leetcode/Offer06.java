d5926e8a8e78a7131898cc4cab4a7c32d7d4a6e2
MatouSakura <1134979484@qq.com>
2022-03-08 15:41:56 +0800

    输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）(借助栈实现)

--- src/Leetcode/Offer06.java
+++ src/Leetcode/Offer06.java
@@ -1,21 +1,35 @@
 package Leetcode;
 
+import java.util.Stack;
+
 public class Offer06 {
     public int[] reversePrint(ListNode head) {
-        ListNode dummyHead = new ListNode(-1);
+//        ListNode dummyHead = new ListNode(-1);
+//        int count = 0;
+//        while(head != null){
+//            count += 1;
+//            ListNode node = new ListNode(head.val);
+//            node.next = dummyHead.next;
+//            dummyHead.next = node;
+//            head = head.next;
+//        }
+//        int[] ret = new int[count];
+//        dummyHead = dummyHead.next;
+//        for (int i = 0; i < count; i++) {
+//            ret[i] = dummyHead.val;
+//            dummyHead = dummyHead.next;
+//        }
+//        return ret;
+        Stack<Integer> s = new Stack<>();
         int count = 0;
         while(head != null){
-            count += 1;
-            ListNode node = new ListNode(head.val);
-            node.next = dummyHead.next;
-            dummyHead.next = node;
+            s.push(head.val);
             head = head.next;
+            count++;
         }
         int[] ret = new int[count];
-        dummyHead = dummyHead.next;
         for (int i = 0; i < count; i++) {
-            ret[i] = dummyHead.val;
-            dummyHead = dummyHead.next;
+            ret[i] = s.pop();
         }
         return ret;
     }
