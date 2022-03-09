ba315905086a5f1fe3fc9e2c8357dfcea098ac6a
MatouSakura <1134979484@qq.com>
2022-03-09 19:53:35 +0800

    请实现一个函数，把字符串 s 中的每个空格替换成"%20"。

--- src/Offer/Offer05.java
+++ src/Offer/Offer05.java
@@ -2,6 +2,16 @@ package Offer;
 
 public class Offer05 {
     public String replaceSpace(String s) {
-        return s.replace(" ","%20");
+        //return s.replace(" ","%20");
+        StringBuilder sb = new StringBuilder();
+        char[] ch = s.toCharArray();
+        for (int i = 0; i < ch.length; i++) {
+            if(ch[i] == ' '){
+                sb.append("%20");
+            }else{
+                sb.append(ch[i]);
+            }
+        }
+        return sb.toString();
     }
 }
