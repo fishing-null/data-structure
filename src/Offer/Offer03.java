b188463841521295470505d80ace876040859640
MatouSakura <1134979484@qq.com>
2022-03-10 10:40:26 +0800

    找出数组中重复的数字。
    在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。(HashSet)

--- src/Offer/Offer03.java
+++ src/Offer/Offer03.java
@@ -1,17 +1,28 @@
 package Offer;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class Offer03 {
     public int findRepeatNumber(int[] nums) {
-        Map<Integer,Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i],1));
-        }
-        for(int i: map.keySet()){
-            if(map.get(i) > 1){
+//        Map<Integer,Integer> map = new HashMap<>();
+//        for (int i = 0; i < nums.length; i++) {
+//            map.put(nums[i], map.getOrDefault(nums[i],1));
+//        }
+//        for(int i: map.keySet()){
+//            if(map.get(i) > 1){
+//                return i;
+//            }
+//        }
+//        return -1;
+        Set<Integer> set = new HashSet<>();
+        for(int i:nums){
+            if(set.contains(i)){
                 return i;
+            } else{
+                set.add(i);
             }
         }
         return -1;
