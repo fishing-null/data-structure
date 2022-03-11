834dd1760744149e96dd6f51aea9ad2029aa2a79
MatouSakura <1134979484@qq.com>
2022-03-11 09:07:36 +0800

    把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。
    给你一个可能存在 重复 元素值的数组 numbers ，它原来是一个升序排列的数组，并按上述情形进行了一次旋转。请返回旋转数组的最小元素。例如，数组 [3,4,5,1,2] 为 [1,2,3,4,5] 的一次旋转，该数组的最小值为1。  

---
+++ src/Offer/Offer11.java
@@ -0,0 +1,12 @@
+package Offer;
+
+public class Offer11 {
+    public int minArray(int[] numbers) {
+        for (int i = 0; i < numbers.length-1; i++) {
+            if(numbers[i+1]<numbers[i]){
+                return numbers[i+1];
+            }
+        }
+        return numbers[0];
+    }
+}
