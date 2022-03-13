d3c6612a1d670c3a869b30cd13dcfa494d167a41
MatouSakura <1134979484@qq.com>
2022-03-13 20:17:25 +0800

    从上到下打印出二叉树的每个节点，同一层的节点按照从左到右的顺序打印。
    例如:
    给定二叉树: [3,9,20,null,null,15,7],

---
+++ src/Offer/Offer32.java
@@ -0,0 +1,35 @@
+package Offer;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Offer32 {
+    public int[] levelOrder(TreeNode root) {
+        if(root == null){
+            return new int[0];
+        }
+        List<Integer> ret = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                ret.add(node.val);
+                if(node.left != null){
+                    queue.offer(node.left);
+                }
+                if(node.right != null){
+                    queue.offer(node.right);
+                }
+            }
+        }
+        int[] r = new int[ret.size()];
+        for (int i = 0; i < ret.size(); i++) {
+            r[i] = ret.get(i);
+        }
+        return r;
+    }
+}
