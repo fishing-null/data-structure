05964a1e1fe46aec97d0d27bac6cb185c4ac9be7
MatouSakura <1134979484@qq.com>
2022-03-16 23:49:15 +0800

    A binary tree is uni-valued if every node in the tree has the same value.
    Given the root of a binary tree, return true if the given tree is uni-valued, or false otherwise.

---
+++ src/Leetcode/Num965.java
@@ -0,0 +1,17 @@
+package Leetcode;
+
+public class Num965 {
+    public boolean isUnivalTree(TreeNode root) {
+        if(root == null){
+            return true;
+        }
+        //注意短路运算 先判空
+        if(root.left != null && root.val != root.left.val){
+            return false;
+        }
+        if(root.right != null && root.val != root.right.val){
+            return false;
+        }
+        return isUnivalTree(root.left)&&isUnivalTree(root.right);
+    }
+}
