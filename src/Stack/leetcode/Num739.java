ae5f5164a8e217f9e7d0274b2541e9ae5186e322
MatouSakura <1134979484@qq.com>
2022-03-19 11:29:24 +0800

    Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.

---
+++ src/Stack/leetcode/Num739.java
@@ -0,0 +1,18 @@
+package Stack.leetcode;
+
+import java.util.LinkedList;
+
+public class Num739 {
+    public int[] dailyTemperatures(int[] temperatures) {
+        LinkedList<Integer> stack = new LinkedList<>();
+        int[] ret = new int[temperatures.length];
+        for (int i = 0; i < temperatures.length; i++) {
+            while(!stack.isEmpty() && temperatures[i]>temperatures[stack.peek()]){
+                int temp = stack.pop();
+                ret[temp] = i-temp;
+            }
+            stack.push(i);
+        }
+        return ret;
+    }
+}
