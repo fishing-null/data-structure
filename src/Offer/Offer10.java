2b271a4ac35a18ee38bc9f3e3784a3d640dd45fe
MatouSakura <1134979484@qq.com>
2022-03-23 13:18:46 +0800

    写一个函数，输入 n ，求斐波那契（Fibonacci）数列的第 n 项（即 F(N)）。(dp)

--- src/Offer/Offer10.java
+++ src/Offer/Offer10.java
@@ -1,17 +1,33 @@
 package Offer;
 
 public class Offer10 {
-    public int fib(int n) {
+    public static void main(String[] args) {
+        fib(7);
+    }
+    public static int fib(int n) {
+//        if(n == 0){
+//            return 0;
+//        }
+//        int a = 0;
+//        int b = 1;
+//        for(int i = 2;i <= n;i++){
+//            int tmp = (a+b)%1000000007;
+//            a = b;
+//            b = tmp;
+//        }
+//        return b;
         if(n == 0){
             return 0;
         }
-        int a = 0;
-        int b = 1;
-        for(int i = 2;i <= n;i++){
-            int tmp = (a+b)%1000000007;
-            a = b;
-            b = tmp;
+        if(n == 1){
+            return 1;
         }
-        return b;
+        int[] F = new int[n+1];
+        F[0] = 0;
+        F[1] = 1;
+        for(int i = 2;i<n+1;i++){
+            F[i] = (F[i-1] + F[i-2])%1000000007;
+        }
+        return F[n];
     }
 }
