350a0881978018ed1c7faf49e5a3f99d01cda9da
MatouSakura <1134979484@qq.com>
2022-03-23 19:00:01 +0800

    rename

---
+++ src/seqList/leetcode/ListNode.java
@@ -0,0 +1,6 @@
+package seqList.leetcode;
+  public class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
---
+++ src/seqList/leetcode/Offer25.java
@@ -0,0 +1,32 @@
+package seqList.leetcode;
+
+public class Offer25 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null){
+            return l2;
+        }
+        if(l2 == null){
+            return l1;
+        }
+        ListNode dummyHead = new ListNode(-1);
+        ListNode last = dummyHead;
+        while (l1 != null && l2 != null){
+            if(l1.val <= l2.val){
+                last.next = l1;
+                last = l1;
+                l1 = l1.next;
+            }else{
+                last.next = l2;
+                last = l2;
+                l2 = l2.next;
+            }
+        }
+        if(l1 == null){
+            last.next = l2;
+        }
+        if(l2 == null){
+            last.next = l1;
+        }
+        return dummyHead.next;
+    }
+}
