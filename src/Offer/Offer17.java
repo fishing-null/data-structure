551bcdd212e25dd427500f5f9e367eae4709bbe1
MatouSakura <1134979484@qq.com>
2022-03-23 12:59:18 +0800

    输入数字 n，按顺序打印出从 1 到最大的 n 位十进制数。比如输入 3，则打印出 1、2、3 一直到最大的 3 位数 999。

---
+++ src/Offer/Offer17.java
@@ -0,0 +1,11 @@
+package Offer;
+
+public class Offer17 {
+    public int[] printNumbers(int n) {
+        int[] ret = new int[(int)Math.pow(10,n)-1];
+        for (int i = 1; i + 1< Math.pow(10,n); i++) {
+            ret[i] = i+1;
+        }
+        return ret;
+    }
+}
