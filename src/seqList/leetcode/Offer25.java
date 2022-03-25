a84ace9f979db52b50ddd37ff06795e3e0d0e0dd
MatouSakura <1134979484@qq.com>
2022-03-25 19:16:41 +0800

    输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。

--- src/seqList/leetcode/Offer25.java
+++ src/seqList/leetcode/Offer25.java
@@ -3,6 +3,32 @@ package seqList.leetcode;
 public class Offer25 {
     //
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+//        if(l1 == null){
+//            return l2;
+//        }
+//        if(l2 == null){
+//            return l1;
+//        }
+//        ListNode dummyHead = new ListNode(-1);
+//        ListNode last = dummyHead;
+//        while (l1 != null && l2 != null){
+//            if(l1.val <= l2.val){
+//                last.next = l1;
+//                last = l1;
+//                l1 = l1.next;
+//            }else{
+//                last.next = l2;
+//                last = l2;
+//                l2 = l2.next;
+//            }
+//        }
+//        if(l1 == null){
+//            last.next = l2;
+//        }
+//        if(l2 == null){
+//            last.next = l1;
+//        }
+//        return dummyHead.next;
         if(l1 == null){
             return l2;
         }
@@ -10,23 +36,25 @@ public class Offer25 {
             return l1;
         }
         ListNode dummyHead = new ListNode(-1);
-        ListNode last = dummyHead;
-        while (l1 != null && l2 != null){
-            if(l1.val <= l2.val){
-                last.next = l1;
-                last = l1;
+        ListNode ret = dummyHead;
+        while(l1!=null && l2!=null){
+            if(l1.val<=l2.val){
+                ListNode cur = new ListNode(l1.val);
+                ret.next = cur;
+                ret = ret.next;
                 l1 = l1.next;
             }else{
-                last.next = l2;
-                last = l2;
+                ListNode cur = new ListNode(l2.val);
+                ret.next = cur;
+                ret = ret.next;
                 l2 = l2.next;
             }
         }
         if(l1 == null){
-            last.next = l2;
+            ret.next = l2;
         }
         if(l2 == null){
-            last.next = l1;
+            ret.next = l1;
         }
         return dummyHead.next;
     }
