8cf062be7652db3c8622d946523525d514ac62f9
MatouSakura <1134979484@qq.com>
2022-03-28 14:20:37 +0800

    Given the root of a binary tree, return the inorder traversal of its nodes' values.

--- src/myBinTree/leetcode/Num94.java
+++ src/myBinTree/leetcode/Num94.java
@@ -2,14 +2,33 @@ package myBinTree.leetcode;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Num94 {
     List<Integer> ret = new ArrayList<>();
     public List<Integer> inorderTraversal(TreeNode root){
+//        if(root == null){
+//            return ret;
+//        }
+//        inorderTraversalHelper(root);
+//        return ret;
+        //迭代写法
         if(root == null){
-            return ret;
+            return new ArrayList<>();
         }
-        inorderTraversalHelper(root);
+        List<Integer> ret = new ArrayList<>();
+        Stack<TreeNode> s = new Stack<>();
+        TreeNode cur = root;
+        while(cur != null || !s.isEmpty()){
+            if(cur != null){
+                s.push(cur);
+                cur = cur.left;
+            }else{
+                cur = s.pop();
+                ret.add(cur.val);
+                cur = cur.right;
+            }
+        }
         return ret;
     }
     public void inorderTraversalHelper(TreeNode root){
