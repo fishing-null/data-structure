63b669501eb3ca45873a021ee305758c1557b54f
MatouSakura <1134979484@qq.com>
2022-04-01 19:43:59 +0800

    Given an array of intervals intervals where intervals[i] = [starti, endi], return the minimum number of intervals you need to remove to make the rest of the intervals non-overlapping.

---
+++ src/greedyAlgorithm/leetcode/Num435.java
@@ -0,0 +1,28 @@
+package greedyAlgorithm.leetcode;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Num435 {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        if(intervals.length == 0){
+            return 0;
+        }
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[1]-o2[1];
+            }
+        });
+        int n = intervals.length;
+        int right = intervals[0][1];
+        int ans = 1;
+        for (int i = 1; i < n; i++) {
+            if(intervals[i][0]>=right){
+                ++ans;
+                right = intervals[i][1];
+            }
+        }
+        return n-ans;
+    }
+}
