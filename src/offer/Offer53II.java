87185bb8c131f8f39e2c62677e851a225139b68b
MatouSakura <1134979484@qq.com>
2022-04-02 17:01:34 +0800

    一个长度为n-1的递增排序数组中的所有数字都是唯一的，并且每个数字都在范围0～n-1之内。在范围0～n-1内的n个数字中有且只有一个数字不在该数组中，请找出这个数字。

--- src/offer/Offer53II.java
+++ src/offer/Offer53II.java
@@ -2,13 +2,20 @@ package offer;
 
 public class Offer53II {
     public int missingNumber(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            if(nums[i] == i){
-                continue;
-            }else{
-                return i;
-            }
+//        for (int i = 0; i < nums.length; i++) {
+//            if(nums[i] == i){
+//                continue;
+//            }else{
+//                return i;
+//            }
+//        }
+//        return nums.length;
+        int i = 0, j = nums.length - 1;
+        while(i <= j) {
+            int m = (i + j) / 2;
+            if(nums[m] == m) i = m + 1;
+            else j = m - 1;
         }
-        return nums.length;
+        return i;
     }
 }
