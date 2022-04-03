7f6db36150015ba4f2d4cd10ed7e35c4b53cf105
MatouSakura <1134979484@qq.com>
2022-04-03 20:51:02 +0800

    Given a characters array letters that is sorted in non-decreasing order and a character target, return the smallest character in the array that is larger than target.
    Note that the letters wrap around.
    
    For example, if target == 'z' and letters == ['a', 'b'], the answer is 'a'.

---
+++ src/leetCode/Num744.java
@@ -0,0 +1,15 @@
+package leetCode;
+
+public class Num744 {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int length = letters.length;
+        char nextGreater = letters[0];
+        for (int i = 0; i < length; i++) {
+            if (letters[i] > target) {
+                nextGreater = letters[i];
+                break;
+            }
+        }
+        return nextGreater;
+    }
+}
