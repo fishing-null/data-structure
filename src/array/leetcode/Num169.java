be9eee965d144a5c4330a77ac1224b324b189c57
MatouSakura <1134979484@qq.com>
2022-04-22 19:53:26 +0800

    Given an array nums of size n, return the majority element.
    The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
    
    来源：力扣（LeetCode）
    链接：https://leetcode-cn.com/problems/majority-element

--- src/array/leetcode/Num169.java
+++ src/array/leetcode/Num169.java
@@ -4,23 +4,25 @@ import java.util.Arrays;
 
 public class Num169 {
     public int majorityElement(int[] nums) {
-        if(nums.length == 1){
-            return nums[0];
-        }
+//        if(nums.length == 1){
+//            return nums[0];
+//        }
+//        Arrays.sort(nums);
+//        int i = 0;
+//        int count = 1;
+//        while(i < nums.length -1){
+//            if(nums[i] == nums[i+1]){
+//                count++;
+//                if(count > nums.length/2){
+//                    return nums[i];
+//                }
+//            }else{
+//                count = 1;
+//            }
+//            i++;
+//        }
+//        return -1;
         Arrays.sort(nums);
-        int i = 0;
-        int count = 1;
-        while(i < nums.length -1){
-            if(nums[i] == nums[i+1]){
-                count++;
-                if(count > nums.length/2){
-                    return nums[i];
-                }
-            }else{
-                count = 1;
-            }
-            i++;
-        }
-        return -1;
+        return nums[nums.length/2];
     }
 }
