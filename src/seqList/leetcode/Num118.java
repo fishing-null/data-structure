e03d3536efed64d18da75a4189fed0afe7e07be3
null <1134979484@qq.com>
2022-10-01 11:07:33 +0800

    杨辉三角

---
+++ src/seqList/leetcode/Num118.java
@@ -0,0 +1,28 @@
+package seqList.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Num118 {
+    public static void main(String[] args) {
+
+    }
+
+    public List<List<Integer>> generate(int numRows) {
+        List<Integer> row1 = new ArrayList<>();
+        List<List<Integer>> ret = new ArrayList<>();
+        row1.add(1);
+        ret.add(row1);
+        for(int i = 1;i<numRows;i++){
+            List<Integer> row = new ArrayList<>();
+            row.add(1);
+            for(int j = 1;j <= (i-1);j++){
+                List<Integer> prevRow = ret.get(i-1);
+                row.add(prevRow.get(j-1)+prevRow.get(j));
+            }
+            row.add(1);
+            ret.add(row);
+        }
+        return ret;
+    }
+}
