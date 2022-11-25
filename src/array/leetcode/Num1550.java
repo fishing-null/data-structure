b608739826ba065695c8254a8e097b79986beb1e
MatouSakura <1134979484@qq.com>
2022-11-26 07:38:57 +0800

    给你一个整数数组 arr，请你判断数组中是否存在连续三个元素都是奇数的情况：如果存在，请返回 true ；否则，返回 false 。

---
+++ src/array/leetcode/Num1550.java
@@ -0,0 +1,21 @@
+package array.leetcode;
+
+public class Num1550 {
+    public static void main(String[] args) {
+
+    }
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] % 2 == 1){
+                count++;
+                if(count == 3){
+                    return true;
+                }
+            }else{
+                count = 0;
+            }
+        }
+        return false;
+    }
+}
