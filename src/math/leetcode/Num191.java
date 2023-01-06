65414323774994e9fc167bc7525403b324a6f976
null <1134979484@qq.com>
2023-01-06 16:06:02 +0800

    编写一个函数，输入是一个无符号整数（以二进制串的形式），返回其二进制表达式中数字位数为 '1' 的个数（也被称为汉明重量）。

---
+++ src/math/leetcode/Num191.java
@@ -0,0 +1,12 @@
+package math.leetcode;
+
+public class Num191 {
+    public int hammingWeight(int n) {
+        int count = 0;
+        while(n!=0){
+            n = n&(n-1);
+            count++;
+        }
+        return count;
+    }
+}
