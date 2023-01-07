85855937e566b2aa39674dbe78f289d4e258e94a
null <1134979484@qq.com>
2023-01-07 17:38:50 +0800

    给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
    你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
    
    你可以按任意顺序返回答案。

---
+++ src/hash/leetcode/Num1.java
@@ -0,0 +1,19 @@
+package hash.leetcode;
+
+import java.util.HashMap;
+
+public class Num1 {
+    public static void main(String[] args) {
+
+    }
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(map.containsKey(target-nums[i])){
+                return new int[]{map.get(target-nums[i]),i};
+            }
+            map.put(nums[i],i);
+        }
+        return new int[0];
+    }
+}
