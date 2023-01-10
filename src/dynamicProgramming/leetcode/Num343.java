94e63278636809c66ae73fbd24f3d206bf8446e8
null <1134979484@qq.com>
2023-01-10 21:34:27 +0800

    给定一个正整数 n ，将其拆分为 k 个 正整数 的和（ k >= 2 ），并使这些整数的乘积最大化。
    返回 你可以获得的最大乘积 。

--- src/dynamicProgramming/leetcode/Num343.java
+++ src/dynamicProgramming/leetcode/Num343.java
@@ -2,13 +2,13 @@ package dynamicProgramming.leetcode;
 
 public class Num343 {
     public static void main(String[] args) {
-
+        integerBreak(10);
     }
-    public int integerBreak(int n) {
+    public static int integerBreak(int n) {
         int[] dp = new int[n+1];
         dp[0] = 0;
         dp[1] = 1;
-        for (int i = 2; i < dp.length; i++) {
+        for (int i = 2; i < n; i++) {
             int max = 0;
             for (int j = 0; j < i; j++) {
                 max = Math.max(max,Math.max(j*(i-j),j*dp[i-j]));
