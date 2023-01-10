1b030f69cecd4e8ba4f1025f796688af768b83fc
null <1134979484@qq.com>
2023-01-10 17:41:02 +0800

    相关企业 请你判断一个 9 x 9 的数独是否有效。只需要 根据以下规则 ，验证已经填入的数字是否有效即可。
    数字 1-9 在每一行只能出现一次。
    数字 1-9 在每一列只能出现一次。
    数字 1-9 在每一个以粗实线分隔的 3x3 宫内只能出现一次。（请参考示例图）

---
+++ src/array/leetcode/Num36.java
@@ -0,0 +1,27 @@
+package array.leetcode;
+
+public class Num36 {
+    public static void main(String[] args) {
+
+    }
+    public boolean isValidSudoku(char[][] board) {
+        int[][] rows = new int[9][9];
+        int[][] cals = new int[9][9];
+        int[][][] subboxes = new int[3][3][9];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char c = board[i][j];
+                if(c != '.'){
+                    int index = c - '0' - 1;
+                    rows[i][index]++;
+                    cals[j][index]++;
+                    subboxes[i/3][j/3][index]++;
+                    if(rows[i][index] > 1 || cals[j][index]++ > 1 || subboxes[i/3][j/3][index] > 1){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
