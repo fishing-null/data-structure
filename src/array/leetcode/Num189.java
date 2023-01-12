6703a6e8d16f1033ffffb00d844ed7d44a450b5d
null <1134979484@qq.com>
2023-01-12 20:49:55 +0800

    给你一个数组，将数组中的元素向右轮转 k 个位置，其中 k 是非负数。

--- src/array/leetcode/Num189.java
+++ src/array/leetcode/Num189.java
@@ -5,13 +5,25 @@ public class Num189 {
 
     }
     public void rotate(int[] nums, int k) {
-        int[] ret = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            ret[i] = nums[i];
-        }
-        for (int i = 0; i < nums.length; i++) {
-            int index = (i+k)% nums.length;
-            nums[index] = ret[i];
+        //时间复杂度O(N) 空间复杂度O(N)
+//        int[] ret = new int[nums.length];
+//        for (int i = 0; i < nums.length; i++) {
+//            ret[i] = nums[i];
+//        }
+//        for (int i = 0; i < nums.length; i++) {
+//            int index = (i+k)% nums.length;
+//            nums[index] = ret[i];
+//        }
+        k = k % nums.length;
+        reverse(nums,0,nums.length-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,nums.length-1);
+    }
+    public void reverse(int[] nums,int start,int end){
+        while(start < end){
+            int tmp = nums[start];
+            nums[start++] = nums[end];
+            nums[end--] = tmp;
         }
     }
 }
