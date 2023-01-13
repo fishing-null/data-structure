9f1ca4380624da5a7d7b4bece19a7018a04f4d3b
null <1134979484@qq.com>
2023-01-13 17:31:52 +0800

    update

--- src/stack/nowcoder/JZ31.java
+++ src/stack/nowcoder/JZ31.java
@@ -1,24 +1,24 @@
 package stack.nowcoder;
 
-import java.util.*;
+import java.util.Stack;
 
 public class JZ31 {
     public static void main(String[] args) {
 
     }
     public boolean IsPopOrder(int [] pushA,int [] popA) {
-        int index = 0;
-        Stack<Integer> stack = new Stack<>();
         if(pushA.length == 0){
             return false;
         }
-        for (int i = 0; i < pushA.length; i++) {
-            stack.push(pushA[i]);
-            while(!stack.isEmpty() && stack.peek() == popA[index]){
+        int index = 0;
+        Stack<Integer> s = new Stack<>();
+        for(int i = 0; i< pushA.length;i++){
+            s.push(pushA[i]);
+            while(!s.isEmpty() && s.peek() == popA[index]){
+                s.pop();
                 index++;
-                stack.pop();
             }
         }
-        return stack.isEmpty();
+        return s.isEmpty();
     }
 }
