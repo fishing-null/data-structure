42b01698612cb18bfd5c39e52a9ea8bd3f978de2
null <1134979484@qq.com>
2023-01-15 15:42:51 +0800

    给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。

--- src/leetCode/Num19.java
+++ src/leetCode/Num19.java
@@ -6,7 +6,7 @@ public class Num19 {
         dummyHead.next = head;
         ListNode fast = dummyHead;
         ListNode low = dummyHead;
-        //找到第倒数n+1个节点
+        //找到倒数第n+1个节点
         for (int i = 0; i < n+1; i++) {
             fast = fast.next;
         }
