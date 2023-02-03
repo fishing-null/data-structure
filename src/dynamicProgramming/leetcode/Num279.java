c4aab70448d7e05313baa91e9d28d6cc57612f3d
null <1134979484@qq.com>
2023-02-03 15:29:20 +0800

    一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
    机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
    
    问总共有多少条不同的路径？

---
+++ src/dynamicProgramming/leetcode/Num279.java
@@ -0,0 +1,19 @@
+package dynamicProgramming.leetcode;
+
+public class Num279 {
+    public static void main(String[] args) {
+        Num279 num279 = new Num279();
+        num279.numSquares(13);
+    }
+    public int numSquares(int n) {
+        int[] dp = new int[n+1];
+        for (int i = 1; i < n+1; i++) {
+            int minn = Integer.MAX_VALUE;
+            for (int j = 1; j*j <= i ; j++) {
+                minn = Math.min(minn,dp[i-j*j]);
+            }
+            dp[i] = minn+1;
+        }
+        return dp[n];
+    }
+}
