3d04829bdf9f896a5f82698d8c969ae110ed920f
null <1134979484@qq.com>
2023-03-14 10:14:38 +0800

    给你一根长度为 n 的绳子，请把绳子剪成整数长度的 m 段（m、n都是整数，n>1并且m>1），每段绳子的长度记为 k[0],k[1]...k[m-1] 。请问 k[0]*k[1]*...*k[m-1] 可能的最大乘积是多少？例如，当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18。

--- src/offer/Offer14.java
+++ src/offer/Offer14.java
@@ -7,16 +7,41 @@ public class Offer14 {
 
     }
     public int cuttingRope(int n) {
-        int[] dp = new int[n+1];
-        dp[0] = 0;
-        dp[1] = 0;
-        for (int i = 2; i < n+1; i++) {
-            int max = 0;
-            for (int j = 0; j < i; j++) {
-                max = Math.max(max,Math.max(j*(i-j),j*dp[i-j]));
-            }
-            dp[i] = max;
+//        int[] dp = new int[n+1];
+//        dp[0] = 0;
+//        dp[1] = 0;
+//        for (int i = 2; i < n+1; i++) {
+//            int max = 0;
+//            for (int j = 0; j < i; j++) {
+//                max = Math.max(max,Math.max(j*(i-j),j*dp[i-j]));
+//            }
+//            dp[i] = max;
+//        }
+//        return dp[n];
+        //2.
+        if(n < 2){
+            return 0;
         }
-        return dp[n];
-    }
+        if(n == 2){
+            return 1;
+        }
+        if(n == 3){
+            return 2;
+        }
+        int[] dp = new int[n+1];
+        dp[0] = 0;dp[1] = 1;dp[2] = 2;dp[3] = 3;
+        int max = 0;
+        for (int i = 4; i <= n ; i++) {
+            max = 0;
+            for (int j = 0; j <= i/2 ; j++) {
+                int product = dp[j]*dp[i-j];
+                if(max < product){
+                    max = product;
+                }
+                dp[i] = max;
+            }
+        }
+        max = dp[n];
+        return max;
+}
 }
