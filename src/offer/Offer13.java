56c5d770bce261c1670865561ef615ac021d8fee
null <1134979484@qq.com>
2023-03-16 09:14:23 +0800

    地上有一个m行n列的方格，从坐标 [0,0] 到坐标 [m-1,n-1] 。一个机器人从坐标 [0, 0] 的格子开始移动，它每次可以向左、右、上、下移动一格（不能移动到方格外），也不能进入行坐标和列坐标的数位之和大于k的格子。例如，当k为18时，机器人能够进入方格 [35, 37] ，因为3+5+3+7=18。但它不能进入方格 [35, 38]，因为3+5+3+8=19。请问该机器人能够到达多少个格子？

---
+++ src/offer/Offer13.java
@@ -0,0 +1,60 @@
+package offer;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Offer13 {
+    public static void main(String[] args) {
+
+    }
+    int[] dx = {0,0,-1,1};
+    int[] dy = {1,-1,0,0};
+    public class Position {
+        int x, y;
+
+        public Position(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+        public int getX() {
+            return x;
+        }
+    }
+    public int movingCount(int m, int n, int k) {
+        if(k == 0){
+            return 1;
+        }
+        int count = 0;
+        Position position = new Position(m,n);
+        Queue<Position> queue = new LinkedList<>();
+        boolean[][] flag = new boolean[m][n];
+        queue.offer(new Position(0,0));
+        while (!queue.isEmpty()){
+            Position tmp = queue.poll();
+            int x = tmp.getX(),y = tmp.getY();
+            for (int i = 0; i < 4; i++) {
+                int tx = x+dx[i], ty = y+dy[i];
+                if(tx < 0 || tx >= m || ty < 0 || ty >= n || get(tx)+get(ty) > k|| flag[tx][ty]){
+                    continue;
+                }
+                queue.offer(new Position(tx,ty));
+                flag[tx][ty] = true;
+                count++;
+            }
+        }
+        return count;
+    }
+    private int get(int x){
+        int ret = 0;
+        while(x != 0){
+            ret += x%10;
+            x /= 10;
+        }
+        return ret;
+    }
+}
