b8ec05efbc33295bee45982fae8afbce119a5b9c
null <1134979484@qq.com>
2023-03-21 11:50:52 +0800

    给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
    给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。

---
+++ src/hash/leetcode/Num17.java
@@ -0,0 +1,44 @@
+package hash.leetcode;
+
+import java.util.*;
+
+public class Num17 {
+    public static void main(String[] args) {
+
+    }
+    public List<String> letterCombinations(String digits) {
+        List<String> ret = new ArrayList<String>();
+        if(digits.length() == 0){
+            return ret;
+        }
+        Map<Character,String> phoneMap = new HashMap<Character,String>(){{
+            put('2',"abc");
+            put('3',"def");
+            put('4',"ghi");
+            put('5',"jkl");
+            put('6',"mno");
+            put('7',"pqrs");
+            put('8',"tuv");
+            put('9',"wxyz");
+
+        }
+        };
+        backTrack(ret,phoneMap,digits,0,new StringBuffer());
+        return ret;
+    }
+
+    private void backTrack(List<String> ret, Map<Character, String> phoneMap, String digits, int index, StringBuffer stringBuffer) {
+        if(index == digits.length()){
+           ret.add(stringBuffer.toString());
+        }else {
+            char digit = digits.charAt(index);
+            String letters = phoneMap.get(digit);
+            int lettersCount = letters.length();
+            for (int i = 0; i < lettersCount; i++) {
+                stringBuffer.append(letters.charAt(i));
+                backTrack(ret, phoneMap, digits, index + 1, stringBuffer);
+                stringBuffer.deleteCharAt(index);
+            }
+        }
+    }
+}
