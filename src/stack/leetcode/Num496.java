264746254db16c44a734878957259df2c3ed1e96
null <1134979484@qq.com>
2023-03-27 11:14:56 +0800

    nums1 中数字 x 的 下一个更大元素 是指 x 在 nums2 中对应位置 右侧 的 第一个 比 x 大的元素。
    给你两个 没有重复元素 的数组 nums1 和 nums2 ，下标从 0 开始计数，其中nums1 是 nums2 的子集。
    
    对于每个 0 <= i < nums1.length ，找出满足 nums1[i] == nums2[j] 的下标 j ，并且在 nums2 确定 nums2[j] 的 下一个更大元素 。如果不存在下一个更大元素，那么本次查询的答案是 -1 。
    
    返回一个长度为 nums1.length 的数组 ans 作为答案，满足 ans[i] 是如上所述的 下一个更大元素 。

--- src/stack/leetcode/Num496.java
+++ src/stack/leetcode/Num496.java
@@ -1,5 +1,9 @@
 package stack.leetcode;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+
 public class Num496 {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         //1.暴力法+模拟
@@ -16,5 +20,21 @@ public class Num496 {
 //            ret[i] = j < n ? nums2[j] : -1;
 //        }
 //            return ret;
+        //2.单调栈+哈希表
+        Deque<Integer> deque = new ArrayDeque<>();
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int i = nums2.length; i >= 0 ; i--) {
+            int num = nums2[i];
+            while(!deque.isEmpty() && num >= deque.peek()){
+                deque.pop();
+            }
+            map.put(num,deque.isEmpty()?-1:deque.peek());
+            deque.push(num);
+        }
+        int[] ret = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            ret[i] = map.get(nums1[i]);
+        }
+        return ret;
     }
 }
