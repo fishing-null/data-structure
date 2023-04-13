16dc589857c988267601212e9fb2d5ff8d6941c5
null <1134979484@qq.com>
2023-04-13 11:15:49 +0800

    给定一个整数，编写一个算法将这个数转换为十六进制数。对于负整数，我们通常使用 补码运算 方法。
    注意:
    
    十六进制中所有字母(a-f)都必须是小写。
    十六进制字符串中不能包含多余的前导零。如果要转化的数为0，那么以单个字符'0'来表示；对于其他情况，十六进制字符串中的第一个字符将不会是0字符。 
    给定的数确保在32位有符号整数范围内。
    不能使用任何由库提供的将数字直接转换或格式化为十六进制的方法

---
+++ src/math/leetcode/Num405.java
@@ -0,0 +1,23 @@
+package math.leetcode;
+
+public class Num405 {
+    public static void main(String[] args) {
+        Num405 num405 = new Num405();
+        num405.toHex(26);
+
+    }
+    public String toHex(int num) {
+        if(num == 0){
+            return "0";
+        }
+        StringBuffer sb = new StringBuffer();
+        for (int i = 7; i >= 0; i--) {
+            int val = (num >> (i*4)) & 0xf;
+            if(!(sb.length() == 0 && val == 0)){
+                char c = val <= 9 ? (char) ('0'+val):(char) ('a'+val-10);
+                sb.append(c);
+            }
+        }
+        return sb.reverse().toString();
+    }
+}
