768082ba941d859784c697e118d1dad734f957e1
null <1134979484@qq.com>
2023-04-20 11:38:22 +0800

    给定 m 个数组，每个数组都已经按照升序排好序了。现在你需要从两个不同的数组中选择两个整数（每个数组选一个）并且计算它们的距离。两个整数 a 和 b 之间的距离定义为它们差的绝对值 |a-b| 。你的任务就是去找到最大距离

---
+++ src/array/Num624.java
@@ -0,0 +1,29 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Num624 {
+    public static void main(String[] args) {
+        Num624 num624 = new Num624();
+        List<List<Integer>> list = new LinkedList<>();
+        List<Integer> list1 = new LinkedList<>();
+        list1.add(1);list1.add(5);
+        List<Integer> list2 = new LinkedList<>();
+        list2.add(3);list2.add(4);
+        list.add(list1);list.add(list2);
+        num624.maxDistance(list);
+
+    }
+    public int maxDistance(List<List<Integer>> arrays) {
+        int min = arrays.get(0).get(0),max = arrays.get(0).get(arrays.get(0).size()-1);
+        int ret = 0;
+        for (int i = 1; i < arrays.size(); i++) {
+            ret = Math.max(ret,Math.max(Math.abs(arrays.get(i).get(arrays.get(i).size()-1)-min),Math.abs(max-(arrays.get(i).get(0)))));
+            min = Math.min(min,arrays.get(i).get(0));
+            max = Math.max(max,arrays.get(i).get(arrays.get(i).size()-1));
+        }
+        return ret;
+    }
+}
