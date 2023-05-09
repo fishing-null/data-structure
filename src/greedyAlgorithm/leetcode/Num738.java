7c66499b87e99aa838eed5e1244cb39550717ec7
null <1134979484@qq.com>
2023-05-09 15:04:39 +0800

    当且仅当每个相邻位数上的数字 x 和 y 满足 x <= y 时，我们称这个整数是单调递增的。
    给定一个整数 n ，返回 小于或等于 n 的最大数字，且数字呈 单调递增 。

--- src/greedyAlgorithm/leetcode/Num738.java
+++ src/greedyAlgorithm/leetcode/Num738.java
@@ -3,27 +3,22 @@ package greedyAlgorithm.leetcode;
 public class Num738 {
     public static void main(String[] args) {
         Num738 num738 = new Num738();
-        num738.monotoneIncreasingDigits(332);
+        num738.monotoneIncreasingDigits(668841);
 
     }
     public int monotoneIncreasingDigits(int n) {
         String s = String.valueOf(n);
         int flag = s.length();
-        StringBuffer sb = new StringBuffer();
         char[] ch= s.toCharArray();
-        for (int i = s.length()-1; i > 0 ; i--) {
-            if(s.charAt(i-1) >= s.charAt(i)){
-                ch[i-1] -= 1;
-                flag = i;
+        for (int i = s.length()-2; i >= 0 ; i--) {
+            if(ch[i] > ch[i+1]){
+                ch[i]--;
+                flag = i+1;
             }
         }
         for (int i = flag; i < ch.length; i++) {
             ch[i] = '9';
-
         }
-        for (int i = 0; i < ch.length; i++) {
-            sb.append(ch[i]);
-        }
-        return Integer.valueOf(sb.toString());
+        return Integer.parseInt(String.valueOf(ch));
     }
 }
