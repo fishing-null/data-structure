1ea1b0dd7c707c961463b73fe022c4ff8677171a
MatouSakura <1134979484@qq.com>
2023-05-10 21:27:58 +0800

    给你一个整数数组 nums 和一个整数 k ，按以下方法修改该数组：
    选择某个下标 i 并将 nums[i] 替换为 -nums[i] 。
    重复这个过程恰好 k 次。可以多次选择同一个下标 i 。
    
    以这种方式修改数组后，返回数组 可能的最大和 。

--- src/greedyAlgorithm/leetcode/Num1005.java
+++ src/greedyAlgorithm/leetcode/Num1005.java
@@ -7,21 +7,54 @@ import java.util.stream.IntStream;
 public class Num1005 {
     public static void main(String[] args) {
         Num1005 num1005 = new Num1005();
-        num1005.largestSumAfterKNegations(new int[] {3,-1,0,2},3);
+        num1005.largestSumAfterKNegations(new int[] {2,-3,-1,5,-4},2);
 
     }
     public int largestSumAfterKNegations(int[] nums, int k) {
-        nums = IntStream.of(nums).boxed().sorted((o1,o2) -> Math.abs(o2) - Math.abs(o1)).mapToInt(Integer::intValue).toArray();
+//        nums = IntStream.of(nums).boxed().sorted((o1,o2) -> Math.abs(o2) - Math.abs(o1)).mapToInt(Integer::intValue).toArray();
+//        int len = nums.length;
+//        for (int i = 0; i < len; i++) {
+//            if(nums[i] < 0 && k > 0){
+//                nums[i] = -nums[i];
+//                k--;
+//            }
+//        }
+//        if(k%2 == 1){
+//            nums[len-1] = -nums[len-1];
+//        }
+//        return Arrays.stream(nums).sum();
+        Arrays.sort(nums);
         int len = nums.length;
+        int count = 0;
         for (int i = 0; i < len; i++) {
-            if(nums[i] < 0 && k > 0){
-                nums[i] = -nums[i];
-                k--;
+            if(nums[i] < 0) {
+                count++;
             }
         }
-        if(k%2 == 1){
-            nums[len-1] = -nums[len-1];
+        int ret = 0;
+        if(k >= count){
+            k -= count;
+            for (int i = 0; i < len; i++) {
+                if(nums[i] < 0){
+                    nums[i] = -nums[i];
+                }
+            }
+            Arrays.sort(nums);
+            if(k != 0 && k % 2 == 1){
+                nums[0] = -nums[0];
+            }
+            for (int i = 0; i < len; i++) {
+                ret += nums[i];
+            }
+        }else{
+            for (int i = 0; i < len; i++) {
+                if(i < k){
+                    ret += (-nums[i]);
+                }else{
+                    ret += nums[i];
+                }
+            }
         }
-        return Arrays.stream(nums).sum();
+        return ret;
     }
 }
