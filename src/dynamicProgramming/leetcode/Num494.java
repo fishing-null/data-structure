843bcd2cb0eb03217d3944ee3b42cce1a81f1a3e
null <1134979484@qq.com>
2023-08-30 10:46:04 +0800

    给你一个非负整数数组 nums 和一个整数 target 。
    向数组中的每个整数前添加 '+' 或 '-' ，然后串联起所有整数，可以构造一个 表达式 ：
    
    例如，nums = [2, 1] ，可以在 2 之前添加 '+' ，在 1 之前添加 '-' ，然后串联起来得到表达式 "+2-1" 。
    返回可以通过上述方法构造的、运算结果等于 target 的不同 表达式 的数目。

---
+++ src/dynamicProgramming/leetcode/Num494.java
@@ -0,0 +1,28 @@
+package dynamicProgramming.leetcode;
+
+public class Num494 {
+    public static void main(String[] args) {
+        Num494 num494 = new Num494();
+        num494.findTargetSumWays(new int[] {1,1,1,1,1},3);
+
+    }
+    public int findTargetSumWays(int[] nums, int target) {
+        int sum = 0;
+        for(int num:nums){
+            sum += num;
+        }
+        int diff = sum - target;
+        int left = (sum + target)/2;
+        if(diff < 0 || diff % 2 != 0){
+            return 0;
+        }
+        int[] dp = new int[left + 1];
+        dp[0] = 1;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = left; j >= nums[i] ; j--) {
+                dp[j] = dp[j] + dp[j - nums[i]];
+            }
+        }
+        return dp[left];
+    }
+}
