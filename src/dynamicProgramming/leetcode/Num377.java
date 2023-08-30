516bf798337071e4638432cc95d93f71a0aded9f
null <1134979484@qq.com>
2023-08-30 14:36:03 +0800

    给你一个由 不同 整数组成的数组 nums ，和一个目标整数 target 。请你从 nums 中找出并返回总和为 target 的元素组合的个数。
    题目数据保证答案符合 32 位整数范围。

---
+++ src/dynamicProgramming/leetcode/Num377.java
@@ -0,0 +1,20 @@
+package dynamicProgramming.leetcode;
+
+public class Num377 {
+    public static void main(String[] args) {
+
+    }
+    public int combinationSum4(int[] nums, int target) {
+        int len = nums.length;
+        int[] dp = new int[target+1];
+        dp[0] = 1;
+        for (int i = 0; i <= target; i++) {
+            for (int j = 0; j < len; j++) {
+                if(i >= nums[j]){
+                    dp[i] += dp[i - nums[j]];
+                }
+            }
+        }
+        return dp[target];
+    }
+}
