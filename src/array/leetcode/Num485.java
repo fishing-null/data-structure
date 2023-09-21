2d4a68d184cbc978430e303692de41b78df334d6
MatouSakura <1134979484@qq.com>
2023-09-21 16:32:36 +0800

    给定一个二进制数组 nums ， 计算其中最大连续 1 的个数

--- src/array/leetcode/Num485.java
+++ src/array/leetcode/Num485.java
@@ -7,14 +7,28 @@ public class Num485 {
 
     }
     public int findMaxConsecutiveOnes(int[] nums) {
-        int max = 0;
-        int count = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if(nums[i] == 1){
+//        int max = 0;
+//        int count = 0;
+//        for (int i = 0; i < nums.length; i++) {
+//            if(nums[i] == 1){
+//                count++;
+//                max = Math.max(count,max);
+//            }else {
+//                count = 0;
+//            }
+//        }
+//        return max;
+        int left=0,right=0;
+        int count=0,max=0;
+        while (right < nums.length){
+            if(nums[right] == 1){
+                right++;
                 count++;
                 max = Math.max(count,max);
-            }else {
-                count = 0;
+            }else{
+                count=0;
+                left = ++right;
+                right = left;
             }
         }
         return max;
