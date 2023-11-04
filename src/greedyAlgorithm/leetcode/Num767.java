222e9573be947d19f7a049b40f5a22d7effddca5
MatouSakura <1134979484@qq.com>
2023-11-04 20:07:21 +0800

    给定一个字符串 s ，检查是否能重新排布其中的字母，使得两相邻的字符不同。
    返回 s 的任意可能的重新排列。若不可行，返回空字符串 "" 。

--- src/greedyAlgorithm/leetcode/Num767.java
+++ src/greedyAlgorithm/leetcode/Num767.java
@@ -1,2 +1,32 @@
-package greedyAlgorithm.leetcode;public class Num767 {
+package greedyAlgorithm.leetcode;
+
+public class Num767 {
+    public String reorganizeString(String s) {
+        int[] hash = new int[26];
+        int maxCount = 0;
+        char maxChar = ' ';
+        for (int i = 0; i < s.length(); i++) {
+            if(maxCount < ++hash[s.charAt(i) - 'a']){
+                maxCount = hash[s.charAt(i) - 'a'];
+                maxChar = s.charAt(i);
+            }
+        }
+        int len = s.length();
+        if(maxCount > (len + 1)/2) return "";
+        char[] ret = new char[len];
+        int index = 0;
+        for (int i = 0; i < maxCount; i++) {
+            ret[index] = maxChar;
+            index += 2;
+        }
+        hash[maxChar - 'a' ] = 0;
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < hash[i]; j++) {
+                if(index >= len)index = 1;
+                ret[index] = (char) (i + 'a');
+                index += 2;
+            }
+        }
+        return new String(ret);
+    }
 }
