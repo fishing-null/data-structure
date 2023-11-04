9036e8254c37c5b1e8b9f7632e955c26289f2926
MatouSakura <1134979484@qq.com>
2023-11-04 19:26:05 +0800

    如果连续数字之间的差严格地在正数和负数之间交替，则数字序列称为 摆动序列 。第一个差（如果存在的话）可能是正数或负数。仅有一个元素或者含两个不等元素的序列也视作摆动序列。
    例如， [1, 7, 4, 9, 2, 5] 是一个 摆动序列 ，因为差值 (6, -3, 5, -7, 3) 是正负交替出现的。
    
    相反，[1, 4, 7, 2, 5] 和 [1, 7, 4, 5, 5] 不是摆动序列，第一个序列是因为它的前两个差值都是正数，第二个序列是因为它的最后一个差值为零。
    子序列 可以通过从原始序列中删除一些（也可以不删除）元素来获得，剩下的元素保持其原始顺序。
    
    给你一个整数数组 nums ，返回 nums 中作为 摆动序列 的 最长子序列的长度 。

--- src/greedyAlgorithm/leetcode/Num376.java
+++ src/greedyAlgorithm/leetcode/Num376.java
@@ -1,2 +1,20 @@
-package greedyAlgorithm.leetcode;public class Num376 {
+package greedyAlgorithm.leetcode;
+
+public class Num376 {
+    public static void main(String[] args) {
+        Num376 num376 = new Num376();
+        num376.wiggleMaxLength(new int[] {1,7,4,9,2,5});
+    }
+    public int wiggleMaxLength(int[] nums) {
+        int left = 0;
+        int right = 0;
+        int count = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            right = nums[i + 1] - nums[i];
+            if(right == 0) continue;
+            if(left * right <= 0) count++;
+            left = right;
+        }
+        return count;
+    }
 }
