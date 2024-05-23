837f08cf8f29c7c05c47a9a301c4e483ffecb2e5
MatouSakura <1134979484@qq.com>
2024-05-23 16:15:12 +0800

    柠檬水找零(加注释)

--- src/greedyAlgorithm/leetcode/Num860.java
+++ src/greedyAlgorithm/leetcode/Num860.java
@@ -2,32 +2,38 @@ package greedyAlgorithm.leetcode;
 
 public class Num860 {
     public static void main(String[] args) {
-        int[] bills = new int[] {5,5,10,20,5,5,5,5,5,5,5,5,5,10,5,5,20,5,20,5};
-        lemonadeChange(bills);
 
     }
-    public static boolean lemonadeChange(int[] bills) {
-        if(bills[0] == 10 || bills[0] == 20){
-            return false;
-        }
-        int count5 = 0,count10 = 0;
-        for (int i = 0; i < bills.length; i++) {
+    public boolean lemonadeChange(int[] bills) {
+        if(bills[0] == 10 || bills[0] == 20) return false;
+        //创建数组存储零钱个数
+        int[] money = new int[3];
+        //遍历bills,能成功遍历到最后返回true,否则返回false
+        for(int i = 0;i < bills.length;i++){
+            //判断零钱类型
             if(bills[i] == 5){
-                count5++;
-            }
-            if(bills[i] == 10){
-                count5--;
-                count10++;
-            }
-            if(bills[i] == 20){
-                if(count10 > 0){
-                    count5--;
-                    count10--;
+                money[0] += 1;
+            }else if(bills[i] == 10){
+                money[1] += 1;
+                money[0] -= 1;
+            }else{
+                //零钱为20的情况,优先使用10+5的方式进行找零,不行则使用5+5+5的方式
+                if(money[1] >= 1 && money[0] >= 1){
+                    money[1] -= 1;
+                    money[0] -= 1;
                 }else{
-                    count5 -= 3;
+                    money[0] -= 3;
                 }
+
             }
-            if(count5 < 0 || count10 < 0){
+            //对零钱数量进行检查
+            if(!checkMoney(money)) return false;
+        }
+        return true;
+    }
+    private boolean checkMoney(int[] money){
+        for(int i = 0;i < money.length;i++){
+            if(money[i] < 0){
                 return false;
             }
         }
