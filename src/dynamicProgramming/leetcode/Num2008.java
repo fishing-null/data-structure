e59859a05680c7f96d068166e4bfbe00e46a7d9a
MatouSakura <1134979484@qq.com>
2024-05-23 10:59:05 +0800

    解码方法

--- src/dynamicProgramming/leetcode/Num2008.java
+++ src/dynamicProgramming/leetcode/Num2008.java
@@ -1,2 +1,35 @@
-package dynamicProgramming.leetcode;public class Num2008 {
+package dynamicProgramming.leetcode;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Num2008 {
+    public static void main(String[] args) {
+        Num2008 num2008 = new Num2008();
+        num2008.maxTaxiEarnings(20,new int[][] {{1,6,1},{3,10,2},{10,12,3},{11,12,2},{12,15,2},{13,18,1}});
+
+    }
+    public long maxTaxiEarnings(int n, int[][] rides) {
+        Arrays.sort(rides, (a, b) -> a[1] - b[1]);
+        int m = rides.length;
+        long[] dp = new long[m + 1];
+        for (int i = 0; i < m; i++) {
+            int j = binarySearch(rides, i , rides[i][0]);
+            dp[i + 1] = Math.max(dp[i],dp[j] + rides[i][1] - rides[i][0] + rides[i][2]);
+        }
+        return dp[m];
+    }
+
+    private int binarySearch(int[][] rides, int high, int target) {
+        int low = 0;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (rides[mid][1] > target) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
 }
---
+++ src/dynamicProgramming/leetcode/Num91.java
@@ -0,0 +1,41 @@
+package dynamicProgramming.leetcode;
+
+import com.sun.deploy.util.StringUtils;
+
+public class Num91 {
+    public static void main(String[] args) {
+        Num91 num91 = new Num91();
+        num91.numDecodings("226");
+
+    }
+    public int numDecodings(String s) {
+        //首先将s转换为字符数组
+        char[] ch = s.toCharArray();
+        int[] num = new int[ch.length];
+        //把ch遍历到整数数组中
+        for (int i = 0; i < ch.length; i++) {
+            num[i] = ch[i] - 48;
+        }
+        if(ch.length == 1){
+            return (num[0] <= 9 && num[0] >= 1 ) ? 1 : 0 ;
+        }
+        //创建dp数组并对其进行初始化,dp[i]表示数组i位置上对应的解码方法有多少种
+        int[] dp = new int[s.length()];
+        dp[0] = (num[0] <= 9 && num[0] >= 1 ) ? 1 : 0 ;
+        //判断dp[1]有多少种解码方式
+        if(dp[0] == 1 && num[1] <= 9 && num[1] >=1 ){
+            dp[1] += 1;
+        }
+        dp[1] += ((num[0]*10 + num[1]) >= 10 && (num[0]*10 + num[1]) <= 26) ? 1 : 0;
+        //状态转移方程
+        for (int i = 2; i < s.length(); i++) {
+            if(num[i] <= 9 && num[i] >=1){
+                dp[i] += dp[i-1];
+            }
+            if((num[i-1]*10 + num[i]) <= 26 && (num[i-1]*10 + num[i]) >= 10){
+                dp[i] += dp[i-2];
+            }
+        }
+        return dp[s.length()-1];
+    }
+}
