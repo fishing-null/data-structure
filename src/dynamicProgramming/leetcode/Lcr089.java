e100c262e3a0761fc22744c469d7dafea8bf0471
MatouSakura <1134979484@qq.com>
2024-05-24 18:18:38 +0800

    打家劫舍

---
+++ src/dynamicProgramming/leetcode/Lcr089.java
@@ -0,0 +1,24 @@
+package dynamicProgramming.leetcode;
+
+public class Lcr089 {
+    public static void main(String[] args) {
+
+    }
+    public int rob(int[] nums) {
+        //构造dp数组
+        int n = nums.length;
+        int[] f = new int[n];
+        int[] g = new int[n];
+        //处理边界条件
+        if(n == 0) return 0;
+        //对dp数组进行初始化
+        f[0] = 0;
+        g[0] = nums[0];
+        //状态转移
+        for(int i = 1;i < n;i++){
+            g[i] = nums[i] + f[i-1];
+            f[i] =  Math.max(g[i-1],f[i-1]);
+        }
+        return Math.max(f[n-1],g[n-1]);
+    }
+}
