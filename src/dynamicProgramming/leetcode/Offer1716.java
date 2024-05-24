c001e97a298c75fbad03152ebd3ccb3c77526c9d
MatouSakura <1134979484@qq.com>
2024-05-24 18:03:28 +0800

    按摩师

--- src/dynamicProgramming/leetcode/Offer1716.java
+++ src/dynamicProgramming/leetcode/Offer1716.java
@@ -5,18 +5,23 @@ public class Offer1716 {
 
     }
     public int massage(int[] nums) {
-        int len = nums.length;
-        if(len == 0){
-            return 0;
+
+        //构造dp数组
+        int n = nums.length;
+        //判断处理边界条件
+        if(n == 0) return 0;
+        //选择i位置的最长预约时长
+        int[] f = new int[n];
+        //不选择i位置的最长预约时长
+        int[] g = new int[n];
+        //初始化
+        f[0] = nums[0];
+        g[0] = 0;
+        //状态转移
+        for(int i = 1;i < n;i++){
+            f[i] = nums[i] + g[i-1];
+            g[i] = Math.max(f[i-1],g[i-1]);
         }
-        int dp0 = 0;
-        int dp1 = nums[0];
-        for (int i = 1; i < len; i++) {
-            int tdp0 = Math.max(dp0,dp1);
-            int tdp1 = dp0+nums[i];
-            dp0 = tdp0;
-            dp1 = tdp1;
-        }
-        return Math.max(dp0,dp1);
+        return Math.max(f[n-1],g[n-1]);
     }
 }
