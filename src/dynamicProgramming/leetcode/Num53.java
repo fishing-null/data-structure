9e7904e30bf306f9a609624a9185b8b8a1a2c34e
MatouSakura <1134979484@qq.com>
2024-05-26 21:05:23 +0800

    最大子数组和

--- src/dynamicProgramming/leetcode/Num53.java
+++ src/dynamicProgramming/leetcode/Num53.java
@@ -7,15 +7,19 @@ public class Num53 {
 
     }
     public int maxSubArray(int[] nums) {
-        if(nums.length == 1){
-            return nums[0];
+        //初始化dp数组
+        int n = nums.length;
+        //处理边界值
+        if(n == 1) return nums[0];
+        //dp[i]表示第i个元素结尾时其所有子数组的最大和
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        int maxSum = -1000000;
+        //状态转移
+        for(int i = 1;i < n + 1;i++){
+            dp[i] = Math.max(nums[i-1],dp[i-1] + nums[i-1]);
+            if(dp[i] > maxSum) maxSum = dp[i];
         }
-        for(int i = 1;i<nums.length;i++){
-            if(nums[i-1]>0){
-                nums[i] = nums[i]+nums[i-1];
-            }
-        }
-        Arrays.sort(nums);
-        return nums[nums.length-1];
+        return maxSum;
     }
 }
