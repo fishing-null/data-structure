cd2c8c033eeeab55229ea4679f3d3afd4e4038b0
MatouSakura <1134979484@qq.com>
2024-05-27 13:43:07 +0800

    环形子数组最大和

---
+++ src/dynamicProgramming/leetcode/Num918.java
@@ -0,0 +1,48 @@
+package dynamicProgramming.leetcode;
+
+import java.util.Arrays;
+
+public class Num918 {
+    public static void main(String[] args) {
+
+    }
+    public int maxSubarraySumCircular(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        int ret1 = maxSubArray(nums);
+        int ret2 = sum-minSubArray(nums);
+        return sum == ret2 ? ret1 : Math.max(ret1,ret2);
+    }
+    public int maxSubArray(int[] nums) {
+        //初始化dp数组
+        int n = nums.length;
+        //处理边界值
+        if(n == 1) return nums[0];
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        int maxSum = Integer.MIN_VALUE;
+        //状态转移
+        for(int i = 1;i < n + 1;i++){
+            dp[i] = Math.max(nums[i-1],dp[i-1] + nums[i-1]);
+            if(dp[i] > maxSum) maxSum = dp[i];
+        }
+        return maxSum;
+    }
+    public int minSubArray(int[] nums) {
+        //初始化dp数组
+        int n = nums.length;
+        //处理边界值
+        if(n == 1) return nums[0];
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        int minSum = Integer.MAX_VALUE;
+        //状态转移
+        for(int i = 1;i < n + 1;i++){
+            dp[i] = Math.min(nums[i-1],dp[i-1] + nums[i-1]);
+            if(dp[i] > minSum) minSum = dp[i];
+        }
+        return minSum;
+    }
+}
