6a7aceea0d3234ddc8b311cc4033c8f18d7e725d
MatouSakura <1134979484@qq.com>
2024-08-09 14:55:32 +0800

    旋转数组的最小数字

---
+++ src/binary/BM21.java
@@ -0,0 +1,24 @@
+package binary;
+
+public class BM21 {
+    public static void main(String[] args) {
+        BM21 bm21 = new BM21();
+        bm21.minNumberInRotateArray(new int[] {2,2,2,1,2});
+    }
+    public int minNumberInRotateArray (int[] nums) {
+        // write code here
+        int left = 0;
+        int right = nums.length - 1;
+        while(left < right){
+            int mid = left + (right - left) / 2;
+            if(nums[mid] > nums[right]){
+                left = mid + 1;
+            }else if(nums[mid] == nums[right]){
+                right = right - 1;
+            }else{
+                right = mid;
+            }
+        }
+        return nums[left];
+    }
+}
