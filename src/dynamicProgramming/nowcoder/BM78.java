7fc9f9947033e6805f0db775c68622e78fd993dd
MatouSakura <1134979484@qq.com>
2024-08-15 22:12:18 +0800

    注释

--- src/dynamicProgramming/nowcoder/BM78.java
+++ src/dynamicProgramming/nowcoder/BM78.java
@@ -8,7 +8,9 @@ public class BM78 {
         // write code here
         //构造dp数组
         int n = nums.length;
+        //表示当前不选择第i间房子,能偷到的最大钱数
         int[] f = new int[n];
+        //表示选择第i间房子的,能偷到的最大钱数
         int[] g = new int[n];
         //处理边界条件
         if(n == 0) return 0;
@@ -17,7 +19,9 @@ public class BM78 {
         g[0] = nums[0];
         //状态转移
         for(int i = 1;i < n;i++){
+            //状态转移
             g[i] = nums[i] + f[i-1];
+
             f[i] =  Math.max(g[i-1],f[i-1]);
         }
         return Math.max(f[n-1],g[n-1]);
