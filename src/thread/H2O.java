108534aeef2d2b18b91a147905d5e0bc54895c02
matousakura <1134979484@qq.com>
2024-08-25 16:38:23 +0800

    H2O生成

---
+++ src/thread/H2O.java
@@ -0,0 +1,25 @@
+package thread;
+
+import java.util.concurrent.Semaphore;
+
+class H2O {
+    Semaphore hSemaphore = new Semaphore(2);
+    Semaphore oSemaphore = new Semaphore(0);
+    public H2O() {
+
+    }
+
+    public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
+        hSemaphore.acquire();
+        // releaseHydrogen.run() outputs "H". Do not change or remove this line.
+        releaseHydrogen.run();
+        oSemaphore.release();
+    }
+
+    public void oxygen(Runnable releaseOxygen) throws InterruptedException {
+        oSemaphore.acquire(2);
+        // releaseOxygen.run() outputs "O". Do not change or remove this line.
+        releaseOxygen.run();
+        hSemaphore.release(2);
+    }
+}
