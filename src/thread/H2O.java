dff00164612601a331179287ef284d6db24418e9
matousakura <1134979484@qq.com>
2024-08-25 16:47:57 +0800

    syn锁

--- src/thread/H2O.java
+++ src/thread/H2O.java
@@ -5,21 +5,35 @@ import java.util.concurrent.Semaphore;
 class H2O {
     Semaphore hSemaphore = new Semaphore(2);
     Semaphore oSemaphore = new Semaphore(0);
+    private Object lock = new Object();
+    private volatile int count = 0;
     public H2O() {
 
     }
 
     public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
-        hSemaphore.acquire();
+        synchronized (lock) {
+            while (count == 2){
+                lock.wait();
+            }
+            count++;
+            releaseHydrogen.run();
+            lock.notifyAll();
+        }
         // releaseHydrogen.run() outputs "H". Do not change or remove this line.
-        releaseHydrogen.run();
-        oSemaphore.release();
+
     }
 
     public void oxygen(Runnable releaseOxygen) throws InterruptedException {
-        oSemaphore.acquire(2);
+        synchronized (lock) {
+            while (count != 2){
+                lock.wait();
+            }
+            count = 0;
+            releaseOxygen.run();
+            lock.notifyAll();
+        }
         // releaseOxygen.run() outputs "O". Do not change or remove this line.
-        releaseOxygen.run();
-        hSemaphore.release(2);
+
     }
 }
