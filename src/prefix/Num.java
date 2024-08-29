0423b22f13f54df1df0c61689c0a60b097f8ff81
fishing-null <1134979484@qq.com>
2024-08-29 14:30:38 +0800

    除自身以外数组的乘积

---
+++ src/prefix/Num.java
@@ -0,0 +1,25 @@
+package prefix;
+
+public class Num {
+    public static void main(String[] args) {
+
+    }
+    public int[] productExceptSelf(int[] nums) {
+        int len = nums.length;
+        int[] prefixAmass = new int[len];
+        int[] suffixAmass = new int[len];
+        int[] ret = new int[len];
+        prefixAmass[0] = 1;
+        suffixAmass[len - 1] = 1;
+        for (int i = 1; i < len; i++) {
+            prefixAmass[i] = nums[i - 1] * prefixAmass[i-1];
+        }
+        for (int i = len - 2; i >= 0 ; i--) {
+            suffixAmass[i] = nums[i + 1] * suffixAmass[i+1];
+        }
+        for (int i = 0; i < len; i++) {
+            ret[i] = prefixAmass[i] * suffixAmass[i];
+        }
+        return ret;
+    }
+}
