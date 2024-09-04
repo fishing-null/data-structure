73863afe817780681b824d7764c27c09770c22f1
fishing-null <1134979484@qq.com>
2024-09-04 16:50:11 +0800

    二叉树剪枝

--- src/myBinTree/leetcode/Lcr047.java
+++ src/myBinTree/leetcode/Lcr047.java
@@ -1,4 +1,19 @@
 package myBinTree.leetcode;
 
 public class Lcr047 {
+    public static void main(String[] args) {
+
+    }
+    public TreeNode pruneTree(TreeNode root) {
+        return search(root);
+    }
+    public TreeNode search(TreeNode root){
+        //递归的终止条件,当前节点为空,则返回空
+        if(root == null) return null;
+        root.left = search(root.left);
+        root.right = search(root.right);
+        //剪枝,若当前节点为叶子节点,并且val == 0,则可剪枝
+        if(root.left == null && root.right == null && root.val == 0) root = null;
+        return root;
+    }
 }
